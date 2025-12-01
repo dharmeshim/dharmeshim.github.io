5c865c57c83f89a2eb7b0608cb2c4b52fc7dad7b
DHARMESH PRASAD <dharmeshprasadr@gmail.com>
2025-12-01 23:19:55 +0530

    education color border correction

--- src/screens/Page/sections/EducationSection/EducationSection.tsx
+++ src/screens/Page/sections/EducationSection/EducationSection.tsx
@@ -26,13 +26,13 @@ export const EducationSection = (): JSX.Element => {
           {education.items.map((item, index) => (
             <motion.div
               key={index}
-              className={`group relative border-l-2 border-gray-300 pl-16 md:pl-20 lg:pl-24 hover:border-purple-500 transition-all duration-500 dark:border-gray-600 dark:hover:border-purple-400 ${normalTransition} rounded-r-lg`}
+              className={`group relative border-l-2 border-gray-300 pl-16 md:pl-20 lg:pl-24 hover:border-blue-500 transition-all duration-500 dark:border-gray-600 dark:hover:border-green-400 ${normalTransition} rounded-r-lg`}
               variants={staggerItem}
               whileHover={{ x: 4 }}
             >
               {/* Timeline dot - animated on scroll */}
               <motion.div
-                className="absolute left-0 top-0 w-5 h-5 bg-gray-400 rounded-full border-4 border-white group-hover:bg-purple-500 transition-all duration-500 transform -translate-x-2.5 dark:bg-gray-600 dark:border-black dark:group-hover:bg-purple-400"
+                className="absolute left-0 top-0 w-5 h-5 bg-gray-400 rounded-full border-4 border-white group-hover:bg-blue-500 transition-all duration-500 transform -translate-x-2.5 dark:bg-gray-600 dark:border-black dark:group-hover:bg-green-400"
                 initial={{ scale: 0 }}
                 animate={inView ? { scale: 1 } : { scale: 0 }}
                 transition={{ delay: index * 0.2, type: "spring", stiffness: 260, damping: 20 }}
